bccdb209c6bd55fc90249987ac8c947b8c5c4dc2
William Hart <whart222@gmail.com>
2016-12-29 15:04:58 -0500

    Adding additional newsvendor examples.

---
+++ examples/newsvendor-pulp.py
@@ -0,0 +1,31 @@
+# newsvendor-pyomo.py
+
+from pulp import *
+
+c=1.0
+b=1.5
+h=0.1
+d = {1:15, 2:60, 3:72, 4:78, 5:82}
+
+scenarios = range(1,6)
+
+M = LpProblem("Newsvendor")
+
+x = LpVariable('x', lowBound=0)
+y = LpVariable.dicts('y', scenarios, lowBound=0)
+
+for i in scenarios:
+  M += y[i] >= (c+h)*x + h*d[i]
+  M += y[i] >= (c-b)*x + b*d[i]
+
+M += sum(y[i] for i in scenarios)/5.0
+
+M.solve()
+
+print("Status = %s" % LpStatus[M.status])
+
+print("%s = %f" % (x.name, value(x.varValue)))
+for i in scenarios:
+    print("%s = %f" % (y[i].name, y[i].varValue))
+print("Objective = %f" % value(M.objective))
+
---
+++ examples/newsvendor-pyomo1.py
@@ -0,0 +1,32 @@
+# newsvendor-pyomo.py
+
+from pyomo.environ import *
+
+c=1.0
+b=1.5
+h=0.1
+d = {1:15, 2:60, 3:72, 4:78, 5:82}
+
+scenarios = range(1,6)
+
+M = ConcreteModel()
+M.x = Var(within=NonNegativeReals)
+M.y = Var(scenarios)
+
+M.c = ConstraintList()
+for i in scenarios:
+  M.c.add( M.y[i] >= (c+h)*M.x + h*d[i] )
+  M.c.add( M.y[i] >= (c-b)*M.x + b*d[i] )
+
+M.o = Objective(expr=sum(M.y[i] for i in scenarios)/5.0)
+
+solver = SolverFactory('glpk')
+status = solver.solve(M)
+
+print("Status = %s" % status.solver.termination_condition)
+
+print("%s = %f" % (M.x, value(M.x)))
+for i in scenarios:
+    print("%s = %f" % (M.y[i], value(M.y[i])))
+print("Objective = %f" % value(M.o))
+
