d82b632d42d922f21ea9ded34a8c38d08820a8ef
whart222 <whart222@gmail.com>
2018-12-08 13:06:01 -0700

    Removing references to plugins

--- pyomocontrib_simplemodel/__init__.py
+++ pyomocontrib_simplemodel/__init__.py
@@ -1,10 +1,5 @@
-from pyomo.common.plugin import PluginGlobals
-PluginGlobals.add_env("pyomo")
-
 from pyomocontrib_simplemodel.core import *
 from pyomo.core.base.numvalue import value
 from pyomo.core.expr.current import *
 from pyomo.core.base.set_types import *
 from pyomo.core.util import *
-
-PluginGlobals.pop_env()
